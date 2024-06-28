d57536893f72f959b0b1767528fb1595ec4d051f
Peter Dinis <peterdinis0@pobox.sk>
2024-06-28 17:45:18 +0200

    feat: Init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import HeroWrapper from "./_components/hero/HeroWrapper";
+
+const Homepage: NextPage = () => {
+  return (
+    <>
+      <HeroWrapper />
+    </>
+  )
+}
+
+export default Homepage;
