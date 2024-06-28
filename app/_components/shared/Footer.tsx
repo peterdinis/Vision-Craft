dca7ef25091bd633570470a049010b27f283d67e
Peter Dinis <peterdinis0@pobox.sk>
2024-06-28 22:39:16 +0200

    FOO

--- app/_components/shared/Footer.tsx
+++ app/_components/shared/Footer.tsx
@@ -1,16 +1,30 @@
+"use client"
+
 import { Label } from "@/components/ui/label";
 import { FC } from "react";
 
 const Footer: FC = () => {
+  const servicesScroll = () => {
+    const servicesSection = document.querySelector("#services") as unknown as HTMLElement;
+    servicesSection.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const pricingScroll = () => {
+    const pricingSection = document.querySelector(
+      "#pricing"
+    ) as unknown as HTMLElement;
+    pricingSection.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div className="mx-auto container xl:px-20 lg:px-12 sm:px-6 px-4 py-12">
       <div className="flex flex-col items-center justify-center">
         <Label className="text-3xl font-bold">Vision-Craft</Label>
         <div className="flex flex-wrap sm:gap-10 gap-8 items-center justify-center mt-8">
-          <p className="hover:text-gray-500 dark:text-white text-base cursor-pointer leading-4 text-gray-800">
+          <p onClick={servicesScroll} className="hover:text-gray-500 dark:text-white text-base cursor-pointer leading-4 text-gray-800">
             Services
           </p>
-          <p className="hover:text-gray-500 dark:text-white text-base cursor-pointer leading-4 text-gray-800">
+          <p onClick={pricingScroll} className="hover:text-gray-500 dark:text-white text-base cursor-pointer leading-4 text-gray-800">
             Pricing
           </p>
         </div>
