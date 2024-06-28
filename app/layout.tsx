c9f8898a0950f5de33a037b7f776d49c0ee9ea09
Peter Dinis <peterdinis0@pobox.sk>
2024-06-28 22:18:13 +0200

    feat: Adding dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./_components/shared/Navigation";
+import ThemeProvider from "./_components/shared/providers/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navigation />
-        {children}
+        <ThemeProvider attribute="class">
+          <Navigation />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
