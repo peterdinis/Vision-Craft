1e8436552376c565ded3f81f3d23f720828de860
Peter Dinis <peterdinis0@pobox.sk>
2024-06-29 16:57:03 +0200

    FOO

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,8 +1,9 @@
-import LoginForm from "@/app/_components/auth/LoginForm";
+
+import RegisterForm from "@/app/_components/auth/RegisterForm";
 import { NextPage } from "next";
 
 const RegisterPage: NextPage = () => {
-    return <LoginForm />
+    return <RegisterForm />
 }
 
 export default RegisterPage;
--- app/_components/auth/LoginForm.tsx
+++ app/_components/auth/LoginForm.tsx
@@ -1,10 +1,34 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Label } from "@radix-ui/react-label";
 import { FC } from "react";
+import { Input } from "@/components/ui/input";
 
 const LoginForm: FC = () => {
     return (
-        <>
-        fff
-        </>
+        <div className="flex justify-center align-top mt-20">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input id="email" type="email" placeholder="m@example.com" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full">Sign in</Button>
+          </CardFooter>
+        </Card>
+        </div>
     )
 }
 
