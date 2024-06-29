63f811902606c782a135d5182ab701a710c83a24
Peter Dinis <peterdinis0@pobox.sk>
2024-06-29 16:18:54 +0200

    FOO

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,8 @@
+import LoginForm from "@/app/_components/auth/LoginForm";
+import { NextPage } from "next";
+
+const RegisterPage: NextPage = () => {
+    return <LoginForm />
+}
+
+export default RegisterPage;
---
+++ app/_components/auth/LoginForm.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const LoginForm: FC = () => {
+    return (
+        <>
+        fff
+        </>
+    )
+}
+
+export default LoginForm;
