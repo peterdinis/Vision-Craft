037aa95306d6870648e9ac5121737d4286ff9e48
Peter Dinis <peterdinis0@pobox.sk>
2024-06-29 22:14:37 +0200

    feat: Updating dashboard WIP

--- app/_components/dashboard/DashboardTable.tsx
+++ app/_components/dashboard/DashboardTable.tsx
@@ -1,11 +1,31 @@
-import { FC } from "react";
+'use client';
 
-const DashboardTable: FC = () => {
-    return (
-        <>
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { FC } from 'react';
+import { DataTable } from '../shared/GlobalTable';
+import { columns } from './columns';
 
-        </>
-    )
+interface IDashboardTableProps {
+    title: string;
+    data: any;
 }
 
-export default DashboardTable
+const DashboardTable: FC<IDashboardTableProps> = ({
+    title,
+    data,
+}: IDashboardTableProps) => {
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle className='prose-h1: prose text-center text-xl font-bold dark:text-white'>
+                    {title}
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <DataTable columns={columns} data={data} />
+            </CardContent>
+        </Card>
+    );
+};
+
+export default DashboardTable;
