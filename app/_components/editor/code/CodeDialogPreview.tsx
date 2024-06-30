d774a271ff78dee99527ff4adede1d9f9eb8ebc8
Peter Dinis <peterdinis0@pobox.sk>
2024-06-30 15:02:04 +0200

    feat: Simple dialog modal for for is done

--- app/_components/editor/code/CodeDialogPreview.tsx
+++ app/_components/editor/code/CodeDialogPreview.tsx
@@ -7,19 +7,29 @@ import {
     DialogTrigger,
 } from '@/components/ui/dialog';
 import { FC } from 'react';
+import Header from '../../shared/Header';
+import CodeMirror from '@uiw/react-codemirror';
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
 
 const CodeDialogPreview: FC = () => {
+    const code = "console.log('Code Mirror!');";
+
     return (
         <>
             <Dialog>
                 <DialogTrigger>Code Preview</DialogTrigger>
                 <DialogContent>
                     <DialogHeader>
-                        <DialogTitle>Are you absolutely sure?</DialogTitle>
+                        <DialogTitle>
+                            <Header text='Code Preview' />
+                        </DialogTitle>
                         <DialogDescription>
-                            This action cannot be undone. This will permanently
-                            delete your account and remove your data from our
-                            servers.
+                            <CodeMirror
+                                className='mt-5'
+                                value={code}
+                                height='100px'
+                                theme={vscodeDark}
+                            />
                         </DialogDescription>
                     </DialogHeader>
                 </DialogContent>
